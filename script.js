d1bbcaa22f6c7cdb4b45cc3dd61a2a03925d0c91
Tweetsauce <pobusauce12@gmail.com>
2021-12-24 01:33:57 +0400

    basic dom funcitonality: make it so that old weather info gets deleted when button pressed again

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-let content = document.querySelector('#content')
-let submitButton = document.querySelector('#submit')
-let searchForm = document.querySelector('#input')
+let content = document.querySelector('#content');
+let submitButton = document.getElementById('submit');
+let searchForm = document.getElementById('form');
+
 async function apiCall(location) {
-  let results = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=635d5ac6bf50d18d320d3ac95935507b`)
-  let weatherData = await results.json()
-  console.log(weatherData)
-  console.log(weatherData.name)
-  console.log(weatherData.main.temp)
+  try {
+    let results = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=635d5ac6bf50d18d320d3ac95935507b`,
+      {mode: 'cors'},
+    );
+    let weatherData = await results.json();
+    console.log(weatherData);
+    processData(weatherData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-apiCall('London')
+// Processes data from the API to stuff that i need
+function processData(data) {
+  let name = data.name;
+  let temperature = Math.round(data.main.temp - 273);
+  let weather = data.weather[0].main;
+  let description = data.weather[0].description;
+  let container = document.createElement('div');
+  container.id = 'container';
+  content.appendChild(container);
+
+  container.appendChild(createDiv(`Country: ${name}`));
+  container.appendChild(createDiv(`Temperature: ${temperature} °C`));
+  container.appendChild(createDiv(`Weather: ${weather}`));
+  container.appendChild(createDiv(`Description: ${description}`));
+}
+
+function createDiv(text) {
+  let newDiv = document.createElement('div');
+  newDiv.textContent = text;
+  return newDiv;
+}
+
+function handleSearchInput() {
+    let query = searchForm.elements[0].value;
+    apiCall(query);
+  }
+submitButton.addEventListener('click', handleSearchInput);
