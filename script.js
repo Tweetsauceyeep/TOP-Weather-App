86f6579d93c72f60fe3c5e8ad44080a887528c8d
Tweetsauce <pobusauce12@gmail.com>
2021-12-24 10:49:58 +0400

    added some CSS and weather info gets deleted when button gets pressed again

--- script.js
+++ script.js
@@ -10,36 +10,53 @@ async function apiCall(location) {
     );
     let weatherData = await results.json();
     console.log(weatherData);
-    processData(weatherData);
+    displayWeatherInfo(weatherData);
   } catch (error) {
     console.log(error);
   }
 }
 
-// Processes data from the API to stuff that i need
-function processData(data) {
+// Processes data from the API to stuff that i need and displays it on da page
+function displayWeatherInfo(data) {
+  let header = document.createElement('h1');
+  header.textContent = 'Weather';
+  header.id = 'weatherHeader';
   let name = data.name;
   let temperature = Math.round(data.main.temp - 273);
+  let feelsLike = Math.round(data.main.feels_like - 273)
   let weather = data.weather[0].main;
   let description = data.weather[0].description;
   let container = document.createElement('div');
   container.id = 'container';
   content.appendChild(container);
 
+  container.appendChild(header);
   container.appendChild(createDiv(`Country: ${name}`));
   container.appendChild(createDiv(`Temperature: ${temperature} °C`));
+  container.appendChild(createDiv(`Feels Like: ${feelsLike} °C`))
   container.appendChild(createDiv(`Weather: ${weather}`));
   container.appendChild(createDiv(`Description: ${description}`));
 }
 
 function createDiv(text) {
   let newDiv = document.createElement('div');
+  newDiv.classList.add('weatherItem');
   newDiv.textContent = text;
   return newDiv;
 }
 
 function handleSearchInput() {
-    let query = searchForm.elements[0].value;
+  let query = searchForm.elements[0].value;
+  if (content.innerHTML === '') {
+    apiCall(query);
+  } else {
+    removeWeatherDiv(content);
     apiCall(query);
   }
+}
+
 submitButton.addEventListener('click', handleSearchInput);
+
+function removeWeatherDiv(containerDiv) {
+  containerDiv.textContent = '';
+}
